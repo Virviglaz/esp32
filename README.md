d31da2cd309295b3290cd173785af9feeef6fe11
Pavel Nadein <pavelnadein@gmail.com>
2020-10-24 13:47:15 +0300

    [i2c] fixes
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # esp32
 esp32 drivers
+
+idf_component_register(SRCS
+	"[sources list].c"
+INCLUDE_DIRS "."
+REQUIRES esp32_libs)	<- https://github.com/Virviglaz/esp32.git
+#PRIV_REQUIRES i2c)	<-idf drivers
--- include/esp32_i2c.h
+++ include/esp32_i2c.h
@@ -57,7 +57,7 @@ esp_err_t i2c_init(i2c_port_t i2c_num, int sda, int scl, uint32_t freq);
 esp_err_t i2c_read_reg(i2c_port_t i2c_num,
 	uint8_t i2c_addr, uint8_t i2c_reg, uint8_t* data_rd, size_t size);
 
-esp_err_t i2c_master_write_slave_reg(i2c_port_t i2c_num,
+esp_err_t i2c_write_reg(i2c_port_t i2c_num,
 	uint8_t i2c_addr, uint8_t i2c_reg, uint8_t* data_wr, size_t size);
 
 #ifdef __cplusplus
